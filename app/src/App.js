d55c283d6ad07548c24627d9a69257a523d1773f
Pravendra Singh <hackpravj@gmail.com>
2018-03-05 02:51:57 +0530

    update: contact details and GitHub link

--- app/src/App.js
+++ app/src/App.js
@@ -21,8 +21,16 @@ class App extends Component {
 
   // IndianGrounds
   render() {
+    const pStyle = {
+      textAlign: 'center',
+      width: 'max-content',
+      margin: 'auto',
+      marginTop: '40px',
+    };
+
     return (
         <div>
+          <p style={pStyle}><a href="https://hackpravj.com" target="_blank">Pravendra Singh</a> <a href="https://github.com/pravj/data-artistry-2018" target="_blank">(GitHub)</a></p>
           <IndianGrounds/>
           <ChangeSlope/>
           <MostDismissals/>
